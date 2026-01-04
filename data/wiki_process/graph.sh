34f4f2aa4ac5a7532e946db7044d88d3aa4384b1
xiaojunqian <148883389+xiaojunqian@users.noreply.github.com>
2026-01-04 14:24:11 +0800

    Delete data/wiki_process/graph.sh

--- data/wiki_process/graph.sh
+++
@@ -1,9 +0,0 @@
-python graph_spacy_R.py \
-    --wiki_data_path "/mnt/cxzx/share/xiaojunqian/data/20231101.en" \
-    --output_path "/mnt/cxzx/share/xiaojunqian/data/wiki_test_output_333" \
-    --num_files 5 \
-    --num_samples 300000 \
-    --num_paths_to_generate 50000 \
-    --num_hops 2 \
-    --gpus "0,1" \
-    --processes_per_gpu 15
